c5ad0eb73be63afc389e69bd93e59db9c7e52563
Rakesh <rakesh.bandari@hitachivantara.com>
2021-01-06 23:46:47 -0800

    CR0000: Deleted deployment folder. REVIEWER:Rakesh

--- deployment/config/examples/hostgroup/init_hg_env.sh
+++
@@ -1,4 +0,0 @@
-sh ../deploy/crds/deploy_crds.sh
-kubectl apply -f ../isp/infra_cr.yaml
-kubectl apply -f ../storagesystems/storage-415056.cr.yaml
-kubectl apply -f ../ucpsystems/ucp.hitachivantara.com_v1alpha1_ucpsystem_cr.yaml
