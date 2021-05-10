315339916da5df43e209451394a5e0225f3edccf
RakeshBandari <Rakesh.Bandari@hitachivantara.com>
2021-05-10 18:12:16 -0500

    checkin the hpe build files

---
+++ build_Ansible-hpe.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+# Make sure these two variables are correct
+Ansible_ROOT=$(pwd)
+Ansible_VER=HPE_XP_Storage_Ansible-02.2.0
+
+MasterProject="HivROMultiPlatformWebService/HivROMultiPlatformWebService.Ans.csproj"
+#ProductionOutDir=${Ansible_ROOT}/HivROMultiPlatformWebService/HivROMultiPlatformWebService
+# Build Arch
+BuildArch="x64"
+# Build mode: release or debug
+BUILD_MODE=$1
+echo "Build Mode: $BUILD_MODE"
+
+# function PublishTarget
+# {
+	# echo "++++++++++++++++++++++Building vRO ++++++++++++++++++++++++++++"
+	# dotnet clean ${Ansible_ROOT}/HivROMultiPlatformWebService/${MasterProject}
+	# mkdir -p $ProductionOutDir/Ansible
+	# if [ "$BUILD_MODE" = "Release" ]; then
+        	# echo "Release Build ..."
+		# dotnet publish ${Ansible_ROOT}/HivROMultiPlatformWebService/${MasterProject} -o $ProductionOutDir/Ansible -c Release > build.log
+	# elif [ "$BUILD_MODE" = "Debug" ]; then 
+        	# echo "Debug Build ..."
+		# dotnet publish ${Ansible_ROOT}/HivROMultiPlatformWebService/${#MasterProject} -o $ProductionOutDir/Ansible -c Debug > build.log
+	# fi
+	# response=`echo $?`
+        # if [ $response -ne 0 ]; then
+	   # echo "dotnet publish command failed. please see build.log for details."
+           # exit 1
+        # fi
+
+	# echo "+++++++++++++++++++Done+++++++++++++++++++++++++++++"
+# }
+
+
+function buildDoLogin
+{
+	rm -rf ${Ansible_ROOT}/GO_WORKSPACE
+	mkdir -p ${Ansible_ROOT}/GO_WORKSPACE/src/doLogin
+	
+	# Copy doLogin.go in the source tree under goDoLogin
+	/usr/bin/cp -f ${Ansible_ROOT}/goDoLogin/doLogin.go ${Ansible_ROOT}/GO_WORKSPACE/src/doLogin
+
+	export GOPATH=${Ansible_ROOT}/GO_WORKSPACE
+
+	echo "======================================================================================"
+ 	echo "Start building doLogin"
+ 	echo "======================================================================================"
+	cd ${Ansible_ROOT}/GO_WORKSPACE/src/doLogin
+	go get -u github.com/BurntSushi/toml
+	go get -u github.com/magiconair/properties
+	go get github.com/iancoleman/orderedmap
+	
+	/usr/local/go/bin/go build
+	
+	 response=`echo $?`
+		  if [ $response -ne 0 ]; then
+		    echo "go build command failed. please see the log file for details."
+		    exit 1
+		 fi
+
+        echo "======================================================================================"
+ 	echo "Copy to addLoginToConfigurations, .grains, and .checkLoginConfigurations"
+ 	echo "======================================================================================"
+	/usr/bin/cp -f doLogin ${Ansible_ROOT}/goDoLogin/addLoginToConfigurations
+        /usr/bin/cp -f doLogin ${Ansible_ROOT}/goDoLogin/.grains
+        /usr/bin/cp -f doLogin ${Ansible_ROOT}/goDoLogin/.checkLoginConfigurations
+		
+        echo "======================================================================================"
+ 	echo "Done"
+ 	echo "======================================================================================"
+}
+
+
+function Clean
+{
+	echo "+++++++++++++++++++Cleaned Deployment folder+++++++++++++++++++++++"
+	#for now clean is not done by pulbish we have to do manually.
+	ex="`rm -rf $Ansible_ROOT/Ansible/*`"
+	ex="`rm -f $Ansible_ROOT/Ansible.tar`"
+	response=`echo $?`
+	if [ $response -ne 0 ]; then
+	   echo "cleanup command failed. please see the log file for details."
+           exit 1
+        fi
+	
+        echo "++++++++++++++++++++++++++Done +++++++++++++++++++++++++++"
+}
+
+function GenerateTar
+{
+	echo "++++++++++++++++++++++++++Generating Pacakge for export+++++++++++++++++"
+	#will add version no later
+	#ex="`tar cvfz $ProductionOutDir/vRO.tar.gz $ProductionOutDir/vRO-1.6.0`"
+	ex="`tar cvf $Ansible_ROOT/Ansible.tar $Ansible_ROOT/ansible`"
+        
+        response=`echo $?`
+        if [ $response -ne 0 ]; then
+           echo "tar command failed. please see the log file for details."
+           exit 1
+        fi
+
+	echo "++++++++++++++++++++++++++Done +++++++++++++++++++++++++++"
+}
+
+function CreateCollection
+{
+  echo "++++++++++++++++++++++++++creating collection+++++++++++++++++"
+  mkdir -p ${Ansible_ROOT}/collections/ansible_collections/
+  
+  cd ${Ansible_ROOT}/collections/ansible_collections/
+  ansible-galaxy collection init hpe.xp_storage --force
+	
+  mkdir ${Ansible_ROOT}/collections/ansible_collections/hpe/xp_storage/plugins/modules
+  mkdir ${Ansible_ROOT}/collections/ansible_collections/hpe/xp_storage/plugins/module_utils
+
+  cp -rf ${Ansible_ROOT}/hi_python_sdk/modules/collections/block/* ${Ansible_ROOT}/collections/ansible_collections/hpe/xp_storage/plugins/modules
+  
+  cp -rf ${Ansible_ROOT}/hi_python_sdk/sdk_package/collections/block/* ${Ansible_ROOT}/collections/ansible_collections/hpe/xp_storage/plugins/module_utils
+  
+  cp ${Ansible_ROOT}/ansible/collection_playbooks/galaxy-hpe.yml ${Ansible_ROOT}/collections/ansible_collections/hpe/xp_storage/galaxy.yml
+	
+  cd ${Ansible_ROOT}/collections/ansible_collections/hpe/xp_storage 
+  ansible-galaxy collection  build --force
+	
+  echo "++++++++++++++++++++++++++Done +++++++++++++++++++++++++++"
+}
+
+# function InstallPuma
+# {
+	# for file in ${Ansible_ROOT}/puma-* ; do
+            # if [ -f "$file" ]; then
+               	# echo "File exists...".
+		# cp -f $file /root/rpmbuild/60EB41E9-71D5-4D5C-85E9-7E0BB76698ED.rpm
+                # break;
+            # else
+               	# echo "File doesn't exists...".
+            # fi
+        # done
+
+# }
+
+echo  "Cleaning the build"
+Clean
+# echo  "publish the build"
+# PublishTarget 
+echo  "Generating tar file"
+GenerateTar
+# echo  "Build VI Service"
+buildDoLogin
+# buildVIService
+# InstallPuma
+
+echo Create Collection
+CreateCollection
+
+echo "Creating RPM. "
+
+# Make sure below directories exist
+echo "Making directories..."
+mkdir -p /root/rpmbuild/SPECS
+mkdir -p /root/rpmbuild/RPMS
+mkdir -p /root/rpmbuild/SOURCES
+mkdir -p /root/rpmbuild/RPMS/x86_64/release
+mkdir -p /root/rpmbuild/RPMS/x86_64/debug
+echo "Finished making directories..."
+
+# Clean up
+echo "Cleanin up..."
+rm -rf /root/rpmbuild/${Ansible_VER}
+mkdir -p /root/rpmbuild/${Ansible_VER}
+rm -rf /root/rpmbuild/SPECS/*.spec
+rm -rf /root/rpmbuild/RPMS/x86_64
+rm -rf /root/rpmbuild/ansible
+rm -rf /root/rpmbuild/sdk_package
+rm -rf /root/rpmbuild/hi_python_sdk
+
+# Blackduck issue, do not ship these jars, they only used by walkmod for java coding styling
+# rm -f ${Ansible_ROOT}/Installer_Linux/Utilities/VIService-ApacheLicense/lib/javalang-compiler-2.2.12.jar
+# rm -f ${Ansible_ROOT}/Installer_Linux/Utilities/VIService-ApacheLicense/lib/merger-1.0.1.jar
+# rm -f ${Ansible_ROOT}/Installer_Linux/Utilities/VIService-ApacheLicense/lib/model-checker-1.1.jar
+# rm -f ${Ansible_ROOT}/Installer_Linux/Utilities/VIService-ApacheLicense/lib/walkmod-core-2.3.3.jar
+# rm -f ${Ansible_ROOT}/Installer_Linux/Utilities/VIService-ApacheLicense/lib/walkmod-javalang-plugin-2.6.10.jar
+
+# rm -f ${Ansible_ROOT}/Installer/Utilities/VIService-ApacheLicense/lib/javalang-compiler-2.2.12.jar
+# rm -f ${Ansible_ROOT}/Installer/Utilities/VIService-ApacheLicense/lib/merger-1.0.1.jar
+# rm -f ${Ansible_ROOT}/Installer/Utilities/VIService-ApacheLicense/lib/model-checker-1.1.jar
+# rm -f ${Ansible_ROOT}/Installer/Utilities/VIService-ApacheLicense/lib/walkmod-core-2.3.3.jar
+# rm -f ${Ansible_ROOT}/Installer/Utilities/VIService-ApacheLicense/lib/walkmod-javalang-plugin-2.6.10.jar
+
+echo "Finished cleanin up..."
+
+# Spec file
+cp -rf ${Ansible_ROOT}/spec/*.spec /root/rpmbuild/SPECS/
+#cp -f ${Ansible_ROOT}/Scripts/hv-infra-gateway.service /root/rpmbuild/
+#cp -f ${Ansible_ROOT}/Scripts/vi.service /root/rpmbuild/
+#cp -f ${Ansible_ROOT}/Scripts/open_firewall_ports.sh /root/rpmbuild/
+cp -f ${Ansible_ROOT}/Scripts/create_log_bundle.sh /root/rpmbuild/
+cp -f ${Ansible_ROOT}/Scripts/uninstall.sh /root/rpmbuild/
+cp -f ${Ansible_ROOT}/Scripts/.cleanup.sh /root/rpmbuild/
+cp -f ${Ansible_ROOT}/Scripts/.cleanup4amo.sh /root/rpmbuild/
+#cp -f ${Ansible_ROOT}/Scripts/puma_external.sh /root/rpmbuild/
+cp -f ${Ansible_ROOT}/logger.config /root/rpmbuild/
+cp -f ${Ansible_ROOT}/messages.properties /root/rpmbuild/
+cp -rf ${Ansible_ROOT}/ansible /root/rpmbuild/
+cp -rf ${Ansible_ROOT}/goDoLogin /root/rpmbuild/
+cp -rf ${Ansible_ROOT}/hi_python_sdk/modules /root/rpmbuild/ansible
+cp -rf ${Ansible_ROOT}/hi_python_sdk/sdk_package /root/rpmbuild/
+#cp -rf $Ansible_ROOT/Ansible /root/rpmbuild/
+#cp -rf ${Ansible_ROOT}/Installer_Linux/Utilities/VIService-ApacheLicense /root/rpmbuild/
+rm -rf /root/rpmbuild/${Ansible_VER}/*
+
+if [[ $BUILD_MODE == "debug" ]]; then
+    #Build vRO debug 
+    
+    if [[ -f ${Ansible_ROOT}/Ansible.tar ]]; then
+        # DEBUG version
+        echo "Starting build rpm for debug version..."
+        cp -rf ${Ansible_ROOT}/Ansible.tar /root/rpmbuild/${Ansible_VER}
+        cd /root/rpmbuild/${Ansible_VER}
+        echo "unzip..."
+        tar -xvf Ansible.tar
+        cd /root/rpmbuild
+        echo "untar..."
+        tar -czvf SOURCES/${Ansible_VER}.tar.gz ${Ansible_VER}
+        echo "rpmbuild..."
+        rpmbuild --target=x86_64 -bb -v SPECS/build_rpm-hpe.spec
+        echo "Finished build rpm for debug version..."
+    else
+        echo "${Ansible_ROOT}/HivROMultiPlatformWebService/HivROMultiPlatformWebService/Ansible.tar does not exist!"
+    fi
+else
+    if [[ -f ${Ansible_ROOT}/Ansible.tar ]]; then
+        # RELEASE version
+        echo "Starting build rpm for release version..."
+        cp -rf ${Ansible_ROOT}/Ansible.tar /root/rpmbuild/${Ansible_VER}
+        cd /root/rpmbuild/${Ansible_VER}
+        echo "untar..."
+        tar -xvf Ansible.tar
+        cd /root/rpmbuild
+        echo "untar..."
+        tar -czvf SOURCES/${Ansible_VER}.tar.gz ${Ansible_VER}
+        rpmbuild --target=x86_64 -bb -v SPECS/build_rpm-hpe.spec
+        echo "Finished build rpm for release version..."
+    else
+        echo "${Ansible_ROOT}/Ansible.tar does not exist!"
+    fi
+fi
+
+# Create tar file 
+mkdir -p  ${Ansible_ROOT}/HV_Storage_Ansible
+
+cp -f  /root/rpmbuild/RPMS/x86_64/HPE_XP_Storage_Ansible-*.rpm  ${Ansible_ROOT}/Scripts/install.sh ${Ansible_ROOT}/Scripts/uninstall.sh ${Ansible_ROOT}/Scripts/.cleanup.sh  ${Ansible_ROOT}/Scripts/.cleanup4amo.sh ${Ansible_ROOT}/Scripts/generate-cert.sh  ${Ansible_ROOT}/Scripts/generate-remote-gw-cert.sh  ${Ansible_ROOT}/HPE_XP_Storage_Ansible
+cd $Ansible_ROOT
+
+# change permissions on the shell scripts
+chmod +x  HPE_XP_Storage_Ansible/install.sh
+chmod +x  HPE_XP_Storage_Ansible/generate-cert.sh
+chmod +x  HPE_XP_Storage_Ansible/generate-remote-gw-cert.sh
+chmod +x  HPE_XP_Storage_Ansible/uninstall.sh
+chmod +x  HPE_XP_Storage_Ansible/.cleanup.sh
+chmod +x  HPE_XP_Storage_Ansible/.cleanup4amo.sh
+
+tar -czvf  ${Ansible_VER}.tar.gz  HPE_XP_Storage_Ansible 
---
+++ spec/build_rpm-hpe.spec
@@ -0,0 +1,202 @@
+name:          HPE_XP_Storage_Ansible
+Version:       02.2.0
+Release:       1%{?dist}
+Summary:       The Ansible modules allow the customer to run BLOCK and FILE storage playbooks.
+Vendor:        Hitachi Vantara
+
+Group:         Adapters 
+License:       hiAdapterLicense
+URL:           http://www.hitachivantara.com 
+Source0:       HPE_XP_Storage_Ansible-02.2.0.tar.gz        
+BuildArch:     x86_64
+BuildRoot:	%{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+AutoReqProv:   no 
+
+#BuildRequires:  
+#Requires:       
+
+%description
+HPE Ansible RPM Package
+
+
+%pre
+
+# Check for OS distribution and minimum version
+min_major_ver=1
+min_minor_ver=0
+major_ver=0
+minor_ver=0
+
+if [[ -f /etc/centos-release ]]; then
+    if [[ -x "$(command -v lsb_release)" ]]; then
+        major_ver=$(lsb_release -r | cut -f2 -d: |cut -f1 -d. | sed 's/[ \t]*//g')
+        minor_ver=$(lsb_release -r | cut -f2 -d: |cut -f2 -d. | sed 's/[ \t]*//g')
+    else
+        major_ver=$(cat /etc/centos-release | cut -d" " -f4 | cut -d. -f1)
+        minor_ver=$(cat /etc/centos-release | cut -d" " -f4 | cut -d. -f2)
+    fi
+fi
+
+if [[ "$1" = "1" ]]; then
+    echo "Preparing for initial installation..."
+elif [[ "$1" = "2" ]]; then
+    echo "Preparing for upgrading..."
+    #systemctl stop hv-infra-gateway
+fi
+
+
+%prep
+%setup -q
+
+
+%build
+%define debug_package %{nil}
+%define __jar_repack %{nil}
+%define ansible_root_dir /opt/hpe
+
+%define ansible_dir ansible
+%define vro_sub_dir %{ansible_root_dir}/%{ansible_dir}
+
+
+%install
+rm -rf $RPM_BUILD_ROOT
+install -d $RPM_BUILD_ROOT/
+install -d $RPM_BUILD_ROOT/opt/hpe/ansible
+install -d $RPM_BUILD_ROOT/opt/hpe/ansible/support
+#install -d $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services
+#install -d $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service
+#install -d $RPM_BUILD_ROOT/opt/hpe/ansible/module_utils
+install -d $RPM_BUILD_ROOT/opt/hpe/ansible/playbooks
+#install -d $RPM_BUILD_ROOT/opt/hpe/ansible/modules
+#install -d $RPM_BUILD_ROOT/opt/hpe/ansible/playbooks/block
+#install -d $RPM_BUILD_ROOT/opt/hpe/ansible/playbooks/file
+#install -d $RPM_BUILD_ROOT/opt/hpe/ansible/modules/block
+#install -d $RPM_BUILD_ROOT/opt/hpe/ansible/modules/file
+install -d $RPM_BUILD_ROOT/opt/hpe/ansible/bin
+install -d $RPM_BUILD_ROOT%{ansible_root_dir}
+#install -d /root/rpmbuild/VIService-ApacheLicense $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service 
+
+#services
+install -d $RPM_BUILD_ROOT/etc/systemd/system
+install -d $RPM_BUILD_ROOT/lib/systemd/system
+install -d $RPM_BUILD_ROOT%{ansible_root_dir}/ansible
+
+#install -m 755 /root/rpmbuild/hv-infra-gateway.service $RPM_BUILD_ROOT/etc/systemd/system/hv-infra-gateway.service
+#install -m 755 /root/rpmbuild/hv-infra-gateway.service $RPM_BUILD_ROOT/lib/systemd/system/hv-infra-gateway.service
+#install -m 755 /root/rpmbuild/hv-infra-gateway.service /etc/systemd/system/hv-infra-gateway.service
+#install -m 755 /root/rpmbuild/hv-infra-gateway.service /lib/systemd/system/hv-infra-gateway.service
+#install -m 755 /root/rpmbuild/vi.service $RPM_BUILD_ROOT/etc/systemd/system/vi.service
+#install -m 755 /root/rpmbuild/vi.service $RPM_BUILD_ROOT/lib/systemd/system/vi.service
+#install -m 755 /root/rpmbuild/vi.service /etc/systemd/system/vi.service
+#install -m 755 /root/rpmbuild/vi.service /lib/systemd/system/vi.service
+#install -m 755 /root/rpmbuild/open_firewall_ports.sh $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services
+#install -m 755 /root/rpmbuild/60EB41E9-71D5-4D5C-85E9-7E0BB76698ED.rpm  $RPM_BUILD_ROOT/opt/hpe
+
+# Copy the whole previous directory
+#cp -f -a %{previous_sub_dir} $RPM_BUILD_ROOT%{ansible_root_dir}
+#cp -f -a /root/rpmbuild/Ansible/*  $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services
+cp -f -a /root/rpmbuild/ansible/storage.json $RPM_BUILD_ROOT/opt/hpe/ansible/storage.json
+cp -f -a /root/rpmbuild/ansible/collection_playbooks/block/* $RPM_BUILD_ROOT/opt/hpe/ansible/playbooks
+rm -f                                             $RPM_BUILD_ROOT/opt/hpe/ansible/playbooks/get_support_logs.yml
+cp -f -a /root/rpmbuild/ansible/collection_playbooks/block/get_support_logs.yml $RPM_BUILD_ROOT/opt/hpe/ansible/support
+cp -f -a /root/rpmbuild/uninstall.sh                                 $RPM_BUILD_ROOT/opt/hpe/ansible/support
+
+cp -f -a /root/rpmbuild/goDoLogin/.grains.sh $RPM_BUILD_ROOT/opt/hpe/ansible/bin
+cp -f -a /root/rpmbuild/goDoLogin/checkLoginConfigurations.sh $RPM_BUILD_ROOT/opt/hpe/ansible/bin
+cp -f -a /root/rpmbuild/goDoLogin/.grains    $RPM_BUILD_ROOT/opt/hpe/ansible/bin
+cp -f -a /root/rpmbuild/goDoLogin/addLoginToConfigurations $RPM_BUILD_ROOT/opt/hpe/ansible/bin
+cp -f -a /root/rpmbuild/goDoLogin/storagejson.properties $RPM_BUILD_ROOT/opt/hpe/ansible/bin
+cp -f -a /root/rpmbuild/goDoLogin/storagejson.properties $RPM_BUILD_ROOT/opt/hpe/ansible/
+        
+#cp -f -a /root/rpmbuild/sdk_package/* $RPM_BUILD_ROOT/opt/hpe/ansible/module_utils
+#rm -f                                 $RPM_BUILD_ROOT/opt/hpe/ansible/module_utils/basic.py
+#cp -f -a /root/rpmbuild/VIService-ApacheLicense  $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service
+cp -f -a /root/rpmbuild/open_firewall_ports.sh  $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services
+cp -f -a /root/rpmbuild/logger.config $RPM_BUILD_ROOT/opt/hpe/ansible/
+cp -f -a /root/rpmbuild/messages.properties $RPM_BUILD_ROOT/opt/hpe/ansible/
+cp -f -a /root/rpmbuild/puma_external.sh  $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services
+#cp -f -a /root/rpmbuild/60EB41E9-71D5-4D5C-85E9-7E0BB76698ED.rpm  $RPM_BUILD_ROOT/opt/hpe
+
+# remove jar files for build to resolve blackduck report issues
+#rm -rf $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service/VIService-ApacheLicense/lib/javalang-compiler-2.2.12.jar
+#rm -rf $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service/VIService-ApacheLicense/lib/merger-1.0.1.jar
+#rm -rf $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service/VIService-ApacheLicense/lib/model-checker-1.1.jar
+#rm -rf $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service/VIService-ApacheLicense/lib/walkmod-core-2.3.3.jar
+#rm -rf $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service/VIService-ApacheLicense/lib/walkmod-javalang-plugin-2.6.10.jar
+#rm -rf $RPM_BUILD_ROOT/opt/hpe/ansible/vi_service/VIService-ApacheLicense/lib/javalang-4.7.9.jar
+
+#rm -rf $rpm_build_root/opt/hpe/ansible/storage_management_services/configuration/hilogger-hpe.config
+#rm -rf $rpm_build_root/opt/hpe/ansible/storage_management_services/configuration/ucpsettings.json
+#rm -rf $rpm_build_root/opt/hpe/ansible/storage_management_services/vro_service_restart.sh
+#rm -rf $rpm_build_root/opt/hpe/ansible/storage_management_services/vro_service_start.sh
+#rm -rf $rpm_build_root/opt/hpe/ansible/storage_management_services/vro_service_stop.sh
+#rm -rf $rpm_build_root/opt/hpe/ansible/storage_management_services/hpeappsettings.json
+#rm -rf $rpm_build_root/opt/hpe/ansible/storage_management_services/hpe_version*
+#rm -rf $rpm_build_root/opt/hpe/ansible/storage_management_services/version.txt
+
+#chmod 644 $RPM_BUILD_ROOT/etc/systemd/system/hv-infra-gateway.service
+#chmod 644 $RPM_BUILD_ROOT/lib/systemd/system/hv-infra-gateway.service
+#chmod 644 $RPM_BUILD_ROOT/etc/systemd/system/vi.service
+#chmod 644 $RPM_BUILD_ROOT/lib/systemd/system/vi.service
+#chmod 755 $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services/hv-infra-gateway_service_start.sh
+#chmod 755 $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services/hv-infra-gateway_service_stop.sh
+#chmod 755 $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services/hv-infra-gateway_service_restart.sh
+#chmod 755 $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services/open_firewall_ports.sh
+#chmod 755 $RPM_BUILD_ROOT/opt/hpe/ansible/storage_management_services/puma_external.sh
+chmod 755 $RPM_BUILD_ROOT/opt/hpe/ansible/bin/checkLoginConfigurations.sh
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%dir %{ansible_root_dir}
+%defattr(-,root,root,-)
+%{ansible_root_dir}/*
+/opt/hpe/ansible/
+#/opt/hpe/ansible/storage_management_services/
+
+#/etc/systemd/system/hv-infra-gateway.service
+#/lib/systemd/system/hv-infra-gateway.service
+#/etc/systemd/system/vi.service
+#/lib/systemd/system/vi.service
+#/opt/hpe/ansible/vi_service/
+#/opt/hpe/60EB41E9-71D5-4D5C-85E9-7E0BB76698ED.rpm
+
+%post
+# chmod 755 -R %{ansible_root_dir}
+
+#disable firewall on http, https ports from appsettings.json and 8444
+#/opt/hpe/ansible/storage_management_services/open_firewall_ports.sh
+
+#Service Actions
+#sudo /usr/bin/systemctl daemon-reload
+#sudo /usr/bin/systemctl enable vi
+#sudo /usr/bin/systemctl enable hv-infra-gateway
+#sudo /usr/bin/systemctl start vi
+#sudo /usr/bin/systemctl start hv-infra-gateway
+
+%preun
+if [[ "$1" = "0" ]]; then
+    # Do some tasks prior to uninstall
+    echo "Preparing uninstall..."
+    #systemctl stop hv-infra-gateway
+elif [[ "$1" = "1" ]]; then
+    # Do some tasks prior to upgrade 
+    echo "Preparing uninstall for upgrading..."
+fi
+
+if [[ -d %{ansible_root_dir}/utils/.modules ]]; then
+    cp -f --no-dereference --preserve=links %{ansible_root_dir}/utils/.modules/* %{ansible_root_dir}
+fi
+
+
+%postun
+if [[ "$1" = "0" ]]; then
+    # Do some tasks after uninstallation
+    echo "Perform some post-tasks for uninstallation..."
+elif [[ "$1" = "1" ]]; then
+    # Do some tasks after upgrade
+    echo "Perform some uninstalled post-tasks for upgrading..."
+fi
+
+%changelog
