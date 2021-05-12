18fe170c9ff09a2153fc48b291bfe8797c0ba82f
RakeshBandari <Rakesh.Bandari@hitachivantara.com>
2021-05-11 21:44:44 -0500

    Updated install scripts for Hitachi Ansible

--- Scripts/.cleanup.sh
+++ Scripts/.cleanup.sh
@@ -2,7 +2,6 @@
 
 # Remove old name version if it exists
 /usr/bin/rpm -e Ansible 2>/dev/null
-echo "Uninstalling Infrastructure Management Service..."
 response_o=`echo $?`
 /usr/bin/rpm -e HV_Ansible 2>/dev/null
 response=`echo $?`
@@ -12,9 +11,9 @@ if [ $response -ne 0 ] && [ $response_o -ne 0 ] && [ "$1" != '-q' ]; then
      exit 1
 fi
 
-# Stop VI service.
-PROCESS_NUM=$(ps -ef | grep "VIService" | grep -v "grep" |  awk '{print $2}')
-#echo $PROCESS_NUM
-kill -9 $PROCESS_NUM
+# # Stop VI service.
+# PROCESS_NUM=$(ps -ef | grep "VIService" | grep -v "grep" |  awk '{print $2}')
+# #echo $PROCESS_NUM
+# kill -9 $PROCESS_NUM
 
 rm -rf /opt/hitachi/ansible
--- Scripts/.cleanup4amo.sh
+++ Scripts/.cleanup4amo.sh
@@ -15,24 +15,24 @@ if [ $response -ne 0 ] && [ $response_o -ne 0 ] && [ "$1" != '-q' ]; then
 fi
 
 # uninstall dependency server if vRO started it.
-conf_file=/var/log/hitachi/pumaconf
-if [ -f "$conf_file" ]; then
-echo "Uninstalling Storage Gateway Service..."
-    /usr/bin/rpm -e puma 2>/dev/null
-        response_o=`echo $?`
-    # delete the conf file
-    /bin/rm -rf  $conf_file
-    /bin/rm -rf  /etc/pki/ca-trust/source/anchors/puma.pem
-fi
+# conf_file=/var/log/hitachi/pumaconf
+# if [ -f "$conf_file" ]; then
+# echo "Uninstalling Storage Gateway Service..."
+    # /usr/bin/rpm -e puma 2>/dev/null
+        # response_o=`echo $?`
+    # # delete the conf file
+    # /bin/rm -rf  $conf_file
+    # /bin/rm -rf  /etc/pki/ca-trust/source/anchors/puma.pem
+# fi
 
-response=`echo $?`
-if [ $response -ne 0 ] && [ "$1" != '-q' ]; then
-     echo "dependency service uninstallation failed."
-     exit 1
-fi
+# response=`echo $?`
+# if [ $response -ne 0 ] && [ "$1" != '-q' ]; then
+     # echo "dependency service uninstallation failed."
+     # exit 1
+# fi
 
 	rm -rf /opt/hitachi/ansible >> /dev/null
 	mkdir -p /opt/hitachi
     mv /tmp/ansible.install.amo /opt/hitachi/ansible
-	rm -rf /opt/hitachi/ansible/vi_service/
-	rm -rf /opt/hitachi/ansible/storage_management_services
+	#rm -rf /opt/hitachi/ansible/vi_service/
+	#rm -rf /opt/hitachi/ansible/storage_management_services
